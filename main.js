e80a3a8789007044efa373ebcb2e4faae839c16b
da-uke <138111118+da-uke@users.noreply.github.com>
2024-07-09 18:18:15 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0
+noseY=0
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,550);
@@ -7,14 +12,24 @@ poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
 }
 function draw(){
-    background('#960A98')
+    background('#960A98');
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX,noseY, difference);
+    document.getElementById("square_side").innerHTML = "Width And Height of a Square = " +difference+"px"
 }
 function modelLoaded(){
     console.log('Posenet Is Initialised✅')
 }
-function gotPoses(){
+function gotPoses(results){
     if(results.length > 0)
         {
             console.log(results);
+            noseX = results[0].pose.nose.x;
+            noseY = results[0].pose.nose.y;
+            
+            leftWristX = results[0].pose.leftWrist.x
+            rightWristX = results[0].pose.rightWrist.x
+            difference = floor(leftWristX - rightWristX)
         }
 }
